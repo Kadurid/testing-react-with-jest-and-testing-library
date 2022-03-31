02d6871fad3df8f909b79d653722245e74758dde
Kadurid <kaduridu@gmail.com>
2022-03-31 15:52:47 -0300

    Implementado botão que muda de cor

--- color-button-jest/src/App.js
+++ color-button-jest/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [buttonColor, setButtonColor] = useState({ backgroundColor: "red" });
+  const newButtonColor = buttonColor.backgroundColor === "red"? "blue" : "red";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        style={buttonColor}
+        onClick={() => {
+          setButtonColor({ backgroundColor: newButtonColor });
+        }}
+      >
+        {buttonColor.backgroundColor === "red"
+          ? "Change to blue"
+          : "Change to red"}
+      </button>
     </div>
   );
 }
