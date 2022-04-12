68a7b674fda4fda4c74fb9518af828a9d45b2745
Kadurid <kaduridu@gmail.com>
2022-04-12 17:42:36 -0300

    feat: iniciando paginas

---
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ScoopOption from "./ScoopOption";
+import Row from 'react-bootstrap/Row'
+
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
+  // optionType is 'scoops' or 'toppings'
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => {
+        setItems(response.data);
+      })
+      .catch((error) => {
+        //TODO:handle error
+      });
+  }, [optionType]);
+
+  //TODO: replace 'null' with ToppingOption when available
+  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+
+  const optionItems = items.map((item) => {
+    return (
+      <ItemComponent
+        key={item.name}
+        name={item.name}
+        imagePath={item.imagePath}
+      />
+    );
+  });
+
+  return <Row>{optionItems}</Row>;
+}
